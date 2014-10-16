146df88acc867bdb3ee11c96692529de728f22f6
minhtranglamthi <minhtranglamthi@gmail.com>
2014-10-17 02:00:55 +0800

    Particles Wave

--- Particle Wave/js/script.js
+++ Particle Wave/js/script.js
@@ -1,39 +1,47 @@
+var numOfPars = 532;
+
 function createParticle() {
 	var myParticles = [];
 	var container = $( ".container" );
-	for ( var i = 0; i < 560; i++ ) {
+	for ( var i = 0; i < numOfPars; i++ ) {
 		myParticles.push('<div class="particles" id="p' + i +'"></div>');
 	}
 	container.append( myParticles.join( "" ) );
 }
 
-function makeFlip(id) {
-	$("#"+ id).addClass('wave');
-	setTimeout(function(){$('.particles').removeClass('wave')},400);
+function makeFlip(pos) {
+	$("#p"+ pos).addClass('wave');
+	setTimeout(function(){$('#p'+ pos).removeClass('wave')},400);
 }
 
-function makeWave(position) {
-	$("#p1").trigger('click');
-	/*setTimeout(function () {
+function makeWave(pos) {
+	makeFlip(pos);
+	setTimeout(function() {
 		$('#p' + (pos + 1)).trigger('click');
 		$('#p' + (pos - 1)).trigger('click');
 		$('#p' + (pos - 28)).trigger('click');
 		$('#p' + (pos + 28)).trigger('click');
-	}, 50);*/
+	},50);
+	setTimeout(function () {
+		$('#p' + pos).on('click', function (event) {
+			$(this).off('click');
+			makeFlip(pos);
+		});
+	}, 800);
 }
 
 function waving(event) {
 	var id = $(this).attr('id');
-	makeFlip(id);
+	alert(id);
+	$(this).off('click');
 	makeWave(getPosition(id));
 }
 
 function getPosition(id) {
-	return id.substr(1);
+	return parseInt(id.substr(1));
 }
 
 //used $( document ).ready( handler ) to ensure element is selected after DOM is ready
 $(function(){
     createParticle();
-    $('.particles').on('click', waving);
 });
