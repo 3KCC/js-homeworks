b1e35d204e0966879319b28bf06154969066dbd3
Minh Trang <minhtranglamthi@gmail.com>
2014-10-27 10:22:43 +0800

    Project 11: Type Attack - init

---
+++ Type Attack/js/script.js
@@ -0,0 +1,25 @@
+// number of drops created.
+var nbDrop = 30; 
+
+// function to generate a random number range.
+function randRange( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createChar(i) {
+    if(i>0) {
+        setTimeout(function(){
+            var dropLeft = randRange(560,800);
+            $('#rain_container').append('<div class="drop" id="drop'+i+'"></div>');
+            $('#drop'+i).css('left',dropLeft);
+            i--;
+            createChar(i);},150);
+    }
+}
+
+$(function(){
+	setTimeout(function() {
+		$('.begin').fadeOut(1000);
+	});
+})
